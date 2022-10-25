0a4661e2fb6ac42b547de9317f0b93614703ea69
Alfred Boateng <programmingwithalfred@gmail.com>
2022-10-25 08:25:44 +0000

    Add likes migration table

---
+++ app/models/like.rb
@@ -0,0 +1,12 @@
+class Like < ApplicationRecord
+  belongs_to :post  
+  belongs_to :user, class_name: "User", foreign_key: "author_id"
+  private
+  after_save :update_likes_count
+  def update_likes_count
+    post = self.post
+    post.likes_count += 1
+    post.save
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_063410) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_25_080106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -33,4 +41,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_063410) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "likes", "posts"
 end
