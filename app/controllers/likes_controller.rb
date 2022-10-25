bd0747aabc55404b1e4bd70b233e3c39201bc0e2
Alfred Boateng <programmingwithalfred@gmail.com>
2022-10-25 08:42:58 +0000

    Update likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,19 @@
 class LikesController < ApplicationController
-  # GET /likes/new
-  def new
-    @like = Like.new
+  def create
+    @like = Like.new(like_params)
+
+    respond_to do |format|
+      if @like.save
+        format.html { redirect_to like_url(@like), notice: "Like was successfully created." }
+        format.json { render :show, status: :created, location: @like }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @like.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  
 
   private
     def like_params
