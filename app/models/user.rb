0c1d3c9a5bd8e4e26572c9a3917b7738f972a064
Alfred Boateng <programmingwithalfred@gmail.com>
2022-10-25 06:26:16 +0000

    Add basic validation in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    MAX_LENGTH = 35
+    MIN_LENGTH = 3
+
+    validates :name, presence: true, length: { maximum: MAX_LENGTH }
+    validates :photo, presence: true, length: { minimum: MIN_LENGTH }
+    validates :bio, presence: true, length: { minimum: MIN_LENGTH }
+
 end
