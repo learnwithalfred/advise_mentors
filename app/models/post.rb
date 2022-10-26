d1d6cd8c2aab4a3cc3aa014b4855c9cebb0e3c72
Alfred Boateng <programmingwithalfred@gmail.com>
2022-10-26 20:46:11 +0000

    Add to Posts
    - A method that updates the posts counter for a user.
    - A method which returns the 5 most recent comments for a given post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,16 @@ class Post < ApplicationRecord
   validates :text, presence: true, length: { minimum: MIN_LENGTH }
 
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, dependent: :destroy
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+  
+  after_save :update_posts_counter
+  def update_posts_counter
+    user = self.user
+    user.post_counter = user.posts.count
+    user.save
+  end
 end
