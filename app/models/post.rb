094ef1ea5df66879f97971ccdb718af967f836e6
Alfred Boateng <programmingwithalfred@gmail.com>
2022-10-27 08:24:22 +0000

    Add validations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,15 @@
 class Post < ApplicationRecord
   MIN_LENGTH = 1
+  MAX_LENGTH = 250
 
-  validates :title, presence: true, length: { minimum: MIN_LENGTH }
+  validates :title, presence: true, length: { minimum: MIN_LENGTH, maximum: MAX_LENGTH }
   validates :text, presence: true, length: { minimum: MIN_LENGTH }
 
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  validates :comments_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_count, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
