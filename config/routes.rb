d7185989ccabb72182a107401ae87a506b3e18de
Alfred Boateng <programmingwithalfred@gmail.com>
2022-10-31 11:43:58 +0000

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,4 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   get '/users/:id/posts', to: 'users#posts'
   get '/users/:id/posts/:post_id', to: 'users#post_details'
-  get '/users/:id/comments', to: 'users#comments'
-  get '/users/:id/likes', to: 'users#likes'
-  get '/posts', to: 'posts#index'
-  get '/posts/:id', to: 'posts#show'
-  get '/posts/:id/comments', to: 'posts#comments'
-  get '/posts/:id/likes', to: 'posts#likes'
-  root "users#index"
 end
