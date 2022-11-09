6b9a39968ea6ec100dacb738f4a3e3c55e8f7ecc
Alfred Boateng <programmingwithalfred@gmail.com>
2022-11-09 10:11:21 +0000

    Add more functions to auth

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,20 +9,6 @@ Rails.application.configure do
     Bullet.add_footer    = true
   end
 
-  config.after_initialize do
-    Bullet.enable = true
-    # Bullet.sentry = true
-    Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
-    # Bullet.honeybadger = true
-    # Bullet.bugsnag = true
-    # Bullet.appsignal = true
-    # Bullet.airbrake = true
-    # Bullet.rollbar = true
-    Bullet.add_footer = true
-  end
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Settings specified here will take precedence over those in config/application.rb.
