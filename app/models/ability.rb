98f60ce9a33ea8f4dc9a09248a86ae3ffad650b2
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-21 15:36:09 +0000

    Remove confirmation from user because of hosting

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,10 @@
-class Ability
-  include CanCan::Ability
+# class Ability
+#   include CanCan::Ability
 
-  def initialize(user)
-    can :manage, :all if user.admin?
+#   def initialize(user)
+#     can :manage, :all if user.admin?
 
-    can :destroy, Post, user: user
-    can :destroy, Comment, user:
-  end
-end
+#     can :destroy, Post, user: user
+#     can :destroy, Comment, user:
+#   end
+# end
