8df4256840da43bcc774c651df032ff9e5b23571
Alfred Boateng <programmingwithalfred@gmail.com>
2022-12-21 15:35:29 +0000

    Update authentication status when user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   respond_to :html, :json
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
